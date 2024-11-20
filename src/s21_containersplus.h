a44cab66496d83199de093af08f2de2d9e527176
ark731 <koreev.r@gmail.com>
2024-11-20 16:08:35 +0300

    my part done

---
+++ src/s21_containersplus.h
@@ -0,0 +1,10 @@
+#ifndef S21_CONTAINERSPLUS_H
+#define S21_CONTAINERSPLUS_H
+
+// #include <initializer_list>
+// #include <vector>
+
+// #include "./s21_array/s21_array.h"
+#include "./containersplus/s21_multiset.h"
+
+#endif  // S21_CONTAINERSPLUS_H
